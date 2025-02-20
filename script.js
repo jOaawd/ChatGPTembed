d5913a726bfee1d62e386488c34cb7f671ba341d
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-02-19 16:41:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -93,12 +93,13 @@ function createChatGPTpopup() {
         cursor: "pointer",
         fontSize: "20px",
         margin: "0",
-        height: "25px",
-        width: "25px",
+        height: "30px",
+        width: "30px",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: "50%"
+        borderRadius: "50%",
+        padding: "0" // Remove any padding
     });
 
     closeButton.addEventListener("mouseenter", () => {
